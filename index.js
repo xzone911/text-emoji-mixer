769abeccd24cf5cb8bd23a03ee3aad027178df40
xzone911 <24585323+xzone911@users.noreply.github.com>
2024-03-28 00:01:39 +0800

    fix

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const text=`A purely peer-to-peer version of electronic cash would allow online
 payments to be sent directly from one party to another without going through a
 financial institution. Digital signatures provide part of the solution, but the main
 benefits are lost if a trusted third party is still required to prevent double-spending.
-    We propose a solution to the double-spending problem using a peer-to-peer network.
-    The network timestamps transactions by hashing them into an ongoing chain of
+We propose a solution to the double-spending problem using a peer-to-peer network.
+The network timestamps transactions by hashing them into an ongoing chain of
 hash-based proof-of-work, forming a record that cannot be changed without redoing
 the proof-of-work. The longest chain not only serves as proof of the sequence of
 events witnessed, but proof that it came from the largest pool of CPU power. As
